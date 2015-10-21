0253cab1ee07d0526fe1fcce181593f7a5ab9a42
Adam Hartz <ahartz1@gmail.com>
2015-10-21 17:13:05 -0400

    Add post test, passing

--- functional_test.py
+++ functional_test.py
@@ -8,14 +8,24 @@ def test_get_api_root():
     assert 'http://127.0.0.1:8080/api/todos/' in res.text
 
 
+# Define the url to the todo api for all subsequent tests to use
+todo_url = 'http://127.0.0.1:8080/api/todos/'
+
+
 def test_get_todos_api():
-    res = requests.get('http://127.0.0.1:8080/api/todos')
+    res = requests.get(todo_url)
 
     assert res.status_code == 200
     assert 'eat more sauce' in res.text
 
+
+def test_post_todos_api():
+    task = {'title': 'shovel snow'}
+    res = requests.post(todo_url, data=task)
+
+    assert res.status_code == 201
+    assert 'shovel snow' in res.text
+
+
     # with open('test.txt', 'w') as f:
     #     f.write(res)
-
-if __name__ == '__main__':
-    test_creation_of_todo()
